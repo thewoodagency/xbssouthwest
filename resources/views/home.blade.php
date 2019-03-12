10d5cded1f37d92de187fec0c2294d48fd8e297a
Jay J <mis@thewoodagency.com>
2019-03-12 09:14:01 -0500

    secure asset

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('css/app.css')}}">
 </head>
 <body>
 <div id="app">
@@ -13,6 +13,6 @@
         <app-footer></app-footer>
     </v-app>
 </div>
-<script src="{{asset('js/app.js')}}"></script>
+<script src="{{secure_asset()asset('js/app.js')}}"></script>
 </body>
 </html>
